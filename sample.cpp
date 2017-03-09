1d68a2f5a4b93405a49305b59bd50d16a57a4a11
dung <cucarrot97@gmail.com>
2017-03-10 00:05:13 +0700

    first commit

---
+++ sample.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "teacher.h"
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+	if (argc != 4){
+		cout << "usage: ChamDiem <img1> <img2> <minHessian>" << endl;
+		return -1;
+	}
+
+	Mat img_object = imread(argv[1], IMREAD_GRAYSCALE);
+	Mat img_scene = imread(argv[2], IMREAD_GRAYSCALE);
+
+	if (!img_object.data || !img_scene.data)
+	{
+		cout << "error reading image!" << endl;
+		return -1;
+	}
+
+	teacher a;
+	a.test();							//khong can thiet
+	a.setMinHessian(atoi(argv[3]));		//500
+	a.setSampleSheet(img_object);		//phieu tra loi mau
+	a.setAnswerImage(img_scene);		//anh chup phieu tra loi cua hoc sinh
+	a.chamDiem();						//bat dau cham diem
+}
